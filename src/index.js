f2ed795f97022e616e0e3273a4c1be8cca677416
Shaheryar Akram <38584504+Sherry-94@users.noreply.github.com>
2018-11-04 01:44:05 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+
 const API_KEY = 'AIzaSyB6hOiXRP0otfvNiLTlsUMniJRZCVD-cQk';
 
 class App extends Component {
